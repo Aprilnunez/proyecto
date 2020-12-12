fba86aceb602dfa5d87d7408714655cc38e64c67
Aprilnunez <aprilnunez14@gmail.com>
2020-12-12 17:30:49 -0300

    cambio

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,12 +1,15 @@
 function Hero() {
     
+    const saludar = () => {
+        alert('Hola!');
+    }
 
 
     return (
         <>
             <h1>Bienvenidos a mi tienda</h1>
             <p>Giftbox</p>
-
+            <button onClick={saludar}>Saludar</button>
         </>
     )
 }
