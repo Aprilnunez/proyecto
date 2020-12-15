b7d3c790f54489306c1712074fedbd1799902ef1
Aprilnunez <aprilnunez14@gmail.com>
2020-12-15 02:15:11 -0300

    modificacion

--- src/components/global/Nav/Nav.jsx
+++ src/components/global/Nav/Nav.jsx
@@ -9,7 +9,7 @@ function Nav({titulo, action}) {
                 <h1>Waldorf</h1>
                 <ul>
                     <button onClick={action}> Productos </button>
-                    <button onClick={action}> Como Comprar </button>
+                    <button onClick={action}> Como Comprar.. </button>
                     <button onClick={action}> Contacto </button>
 
 
